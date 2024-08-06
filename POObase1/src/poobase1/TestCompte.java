cd01637df26d107e3a95dcad5b195e4c6ebfa1f4
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-08-06 15:53:49 +0200

    usefeat(POObase1, TestCompte.java, UPDATE): Utilisation de la classe Compte.

--- POObase1/src/poobase1/TestCompte.java
+++ POObase1/src/poobase1/TestCompte.java
@@ -5,6 +5,12 @@ public class TestCompte {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Compte compte = new Compte(2600d);
+        System.out.println("J'ajoute 42 au solde et affiche le nouveau solde.");
+        compte.deposer(42f);
+        compte.afficher();
+        System.out.println("Je retire 2,011115 au solde et affiche le nouveau solde.");
+        compte.retirer(2.011115f);
+        compte.afficher();
     }
 }
