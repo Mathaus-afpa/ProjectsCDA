0f9d083763fccbd4595bd0943f6c091ede1e9465
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-08-06 16:47:52 +0200

    usefeat(POObase3, TestRectangle.java, UPDATE): Utilisation de la classe Rectangle.

--- POObase3/src/poobase3/TestRectangle.java
+++ POObase3/src/poobase3/TestRectangle.java
@@ -5,6 +5,11 @@ public class TestRectangle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Rectangle rectangle1 = new Rectangle(10, 12);
+        Rectangle rectangle2 = new Rectangle(2, 2);
+        Rectangle rectangle3 = new Rectangle(0, 1);
+        rectangle1.afficher();
+        rectangle2.afficher();
+        rectangle3.afficher();
     }
 }
