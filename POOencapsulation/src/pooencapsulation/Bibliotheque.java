730270604ce010a1807247ff3d22d749a179b7aa
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-08-07 09:58:04 +0200

    usefeat(POOencapsulation, Bibliotheque.java, UPDATE): Utilisation de la classe Livre.

--- POOencapsulation/src/pooencapsulation/Bibliotheque.java
+++ POOencapsulation/src/pooencapsulation/Bibliotheque.java
@@ -5,6 +5,24 @@ public class Bibliotheque {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Livre programmerEnJava = new Livre("Claude Delannoy", "Programmer en Java");
+        programmerEnJava.setNbPages(962);
+        Livre javaEnAction = new Livre("O'Reilly", "Java en action");
+        javaEnAction.setNbPages(836);
+        afficherLivre(programmerEnJava);
+        afficherLivre(javaEnAction);
+    }
+    /**
+     * Affiche les informations d'un livre dans la console
+     * @param livre 
+     */
+    private static void afficherLivre(Livre livre) {
+        System.out.print("Le livre \"");
+        System.out.print(livre.getTitre());
+        System.out.print("\" a pour auteur ");
+        System.out.print(livre.getAuteur());
+        System.out.print(" et possede ");
+        System.out.print(livre.getNbPages());
+        System.out.println(" pages.");
     }
 }
