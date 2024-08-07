98de7ee4ae2be6820e6a71b5b612a01074927db7
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-08-07 09:19:27 +0200

    usefeat(POObase4, TestCercle.java, UPDATE): Utilisation des classes Cercle et Point.

--- POObase4/src/poobase4/TestCercle.java
+++ POObase4/src/poobase4/TestCercle.java
@@ -1,10 +1,18 @@
 package poobase4;
 
+import poobase4.geometry.Cercle;
+import poobase4.geometry.Point;
+
 public class TestCercle {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Cercle cercle = new Cercle(13);
+        cercle.setCentre(-1, 2);
+        Point A = new Point(10, -5);
+        Point B = new Point(11, 7);
+        cercle.testAppartenance(A);
+        cercle.testAppartenance(B);
     }
 }
